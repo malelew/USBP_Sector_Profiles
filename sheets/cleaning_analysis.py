e22f47d6ae05b6de6a968b6d0523f00b69b49399
malelew@ucsd.edu <malelew@ucsd.edu>
2018-10-30 14:34:00 -0700

    json in records orient

--- sheets/cleaning_analysis.py
+++ sheets/cleaning_analysis.py
@@ -11,8 +11,10 @@ df = df[df["SECTOR"].isin(southern_sector)]
 
 staffing_df = df[["SECTOR", "AGENT_STAFFING", "YEAR"]]
 apprehensions_df = df[["SECTOR", "APPREHENSIONS", "YEAR"]]
+staffing_apprehensions_df = df[["SECTOR", "AGENT_STAFFING", "APPREHENSIONS", "YEAR"]]
 
-staffing_df.to_json("southern_staffing.json")
-apprehensions_df.to_json("southern_apprehensions.json")
+staffing_df.to_json("southern_staffing.json", orient="records")
+apprehensions_df.to_json("southern_apprehensions.json", orient="records")
+staffing_apprehensions_df.to_csv("sothern_staffing_apprehensions.csv", index=False)
 df.to_csv("handclean_merged_southern.csv", index=False)
 print "done"
